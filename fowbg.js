00d11b4f588c60a1bd70f47ff9a0cc5fc8cac3e8
Yuri <achi@hush.ai>
2021-01-08 15:41:37 +0100

    Update fowbg.js
    * Fixed increased chance of getting a card with alternate art
    * Limited maximum packs to 10
    * Removed horizontal separator below last pack

--- fowbg.js
+++ fowbg.js
@@ -26,36 +26,60 @@ function generate(tgtdata){
   tgt=tgtdata.split("-")
   tgtcluster=data[tgt[0]].name
   tgtset=data[tgt[0]].sets[tgt[1]]
-  console.log("Generating booster for "+tgtset.code)
+  console.log("Generating "+tgtset.code+" booster")
 
   randcards=[]
   //New distribution
   if(["EDL", "AO1", "AO2", "AO3", "PofA", "GITS2045"].indexOf(tgtset.code)>=0){
     //6 commons
     for(j=0; j<6; j++){
-      randcards.push(choice(tgtset.cards["N"]))
+      while(1==1){
+        ncard=choice(tgtset.cards["N"])
+        //Filter alternate art cards (e.g. EDL-005*)
+        //Filter J-ruler cards (e.g. EDL-005J)
+        if (["*", "J"].indexOf(ncard[ncard.length-1])==-1){
+          randcards.push(ncard)
+          break
+        }
+      }
     }
     //1 rare/sr/mr (to-do: No ruler)
-    randcards.push(choice([].concat(tgtset.cards["R"], tgtset.cards["SR"], tgtset.cards["MR"])))
+    while(1==1){
+      ncard=choice([].concat(tgtset.cards["R"], tgtset.cards["SR"], tgtset.cards["MR"]))
+      if (["*", "J"].indexOf(ncard[ncard.length-1])==-1){
+        randcards.push(ncard)
+        break
+      }
+    }
     //1 foil (inc. rulers)
-    randcards.push(choice([].concat(tgtset.cards["N"], tgtset.cards["R"], tgtset.cards["SR"], tgtset.cards["MR"])))
+    while(1==1){
+      ncard=choice([].concat(tgtset.cards["N"], tgtset.cards["R"], tgtset.cards["SR"], tgtset.cards["MR"]))
+      if (["*", "J"].indexOf(ncard[ncard.length-1])==-1){
+        randcards.push(ncard)
+        break
+      }
+    }
   }
-
   console.log("Cards in pack: ", randcards)
   return randcards
 }
 
-//Card fetcher
-
+//Card image fetcher
 function fetch(clusterid, code){
-  code=code.replace("*","").split('-')
-  return "https://www.fowdb.altervista.org/images/thumbs/"+(8-clusterid)+"/"+code[0].toLowerCase()+"/"+code[1].replace("J","")+".jpg"
+  code=code.split('-')
+  return "https://www.fowdb.altervista.org/images/thumbs/"+(8-clusterid)+"/"+code[0].toLowerCase()+"/"+code[1]+".jpg"
 }
 
 //Button listener
 document.getElementById('generate').onclick = function() {
-  selectedset=document.getElementsByName('set')[0].value;
+  selectedset=document.getElementsByName('set')[0].value
+  //Limit packs to 10 to prevent excessive bandwidth use
   packs=Number(document.getElementsByName('packs')[0].value)
+  if (packs>10){
+    document.getElementsByName('packs')[0].value="10"
+    packs=10
+  }
+  //Generate random cards
   cards=[]
   for (i=0; i<packs; i++){
     console.log((i+1)+"/"+packs)
@@ -69,8 +93,8 @@ document.getElementById('generate').onclick = function() {
   //Write HTML
   html=""
   for (i=0; i<cards.length; i++){
-    html+="<img src='"+fetch(tgt[0],cards[i])+"' width=20%>"
-    if ((i+1)%8==0 && i>0 && !sortbyid){
+    html+="<img src='"+fetch(tgt[0],cards[i])+"' width='175px'>"
+    if ((i+1)%8==0 && i>0 && !sortbyid && i!=cards.length-1){
       html+="<hr/>"
     }
   }
